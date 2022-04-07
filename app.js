e2a0bd56665d751ccb97c72e7296cff34630a828
nora kramer <74241562+norakramer1@users.noreply.github.com>
2022-04-07 16:15:47 +0200

    add online poll files

---
+++ app.js
@@ -0,0 +1,125 @@
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 5500;
+ // let ejs = require('ejs');
+const bodyParser = require('body-parser');
+const {v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const express = require('express')
+const app = express()
+
+
+// sets view engine as ejs
+app.set('view engine', 'ejs');
+
+// Tell the views engine/ejs where the template files are stored (Settingname, value)
+app.set('views', './views');
+
+// Hierdoor kan ik CSS en JS bestanden aan de client side uitlezen
+app.use(express.static(__dirname + '/public'));
+
+// Gebruik body-parser om te lezen wat er in POST requests van de form staat
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true,}));
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get("/", (req, res) => {
+  //console.log(res)
+  res.render("home", {
+    pageTitle: "Home",
+  });
+});
+
+// respond with view poll render on viewpoll route
+app.get("/makepoll", (req, res) => {
+  res.render("makepoll", {
+    pageTitle: "Make A New Poll",
+  });
+});
+
+let voteAnswers = [];
+// get form data and post to json file, then render viewpoll view
+app.post("/makepoll", (req, res) => {
+  fs.readFile("statham.json", "utf8", (err, data) => {
+    let pollList = JSON.parse(data);
+    let arr = {
+      id: uuidv4(),
+      vraag: req.body.vraag,
+      antwoordA: req.body.antwoordA,
+      antwoordB: req.body.antwoordB,
+      votes: voteAnswers,
+    };
+    pollList.push(arr);
+    let pollSubmit = JSON.stringify(pollList, null, 2);
+    fs.writeFile("statham.json", pollSubmit, "utf8", (cb) => {});
+  });
+  res.render("home", {
+    pageTitle: "Poll questions",
+  });
+});
+
+// respond with view poll render on viewpoll route
+app.get("/viewpoll", (req, res) => {
+  fs.readFile("statham.json", "utf8", (err, data) => {
+    let pollList = JSON.parse(data);
+    pollList.reverse();
+    // console.log(pollList)
+
+    res.render("viewpoll", { 
+      data: pollList,
+      pageTitle: "View Polls",
+    })
+  })
+ 
+});
+
+let pollVotesA;
+let pollVotesB;
+
+//take poll answers and put results in json file, then render viewanswers
+app.post("/:id", (req, res) => {
+  voteAnswers.push(req.body.answer)
+  // console.log(voteAnswers)
+  fs.readFile("statham.json", "utf8", (err, data) => {
+    let pollList = JSON.parse(data);
+    // console.log(pollList);
+    pollList.forEach(item => {
+      if (item.id === req.params.id) {
+        item.votes.push(req.body.answer);
+    pollVotesA = item.votes.filter(vote => vote === item.antwoordA).length;
+    pollVotesB = item.votes.filter(vote => vote === item.antwoordB).length;
+    // console.log(pollVotesA, pollVotesB)
+      }
+    })
+    let pollAntwoorden = JSON.stringify(pollList, null, 2);
+    fs.writeFile("statham.json", pollAntwoorden, "utf8", (cb) => {});
+
+    res.redirect("viewanswers")
+
+  });
+
+
+});
+
+
+//view poll results on poll results page
+app.get("/viewanswers", (req, res) => {
+  fs.readFile('statham.json', (err, results) => {
+    console.log(pollVotesA, pollVotesB);
+
+ });
+  
+    res.render("viewanswers", {
+    pageTitle: 'poll results',
+    hoeveelheidA: pollVotesA,
+    hoeveelheidB: pollVotesB,
+      // hoeveelheidB: "Poll answers",
+
+    });
+  });
+
+
+// listen to the port
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
